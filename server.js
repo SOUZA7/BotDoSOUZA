419c1d61b7fcb8a784fccae485ed05039ce5e935
SOUZA7 <davidfigsouza@gmail.com>
2021-03-28 19:19:11 -0300

    Creating js file

---
+++ server.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const tmi = require('tmi.js');
+
+const regexpCommand = new RegExp(/^!([a-zA-Z0-9]+)(?:\W+)?(.*)?/);
+
+const client = new tmi.Client({
+	options: { debug: true },
+	connection: { reconnect: true },
+	identity: {
+		username: process.env.TWITCH_BOT_USERNAME,
+		password: process.env.TWITCH_OAUTH_TOKEN,
+	},
+	channels: [ 'souza7' ]
+});
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+	// Ignore echoed messages.
+	if(self) return;
+
+	if(message.toLowerCase() === '!hello') {
+		// "@alca, heya!"
+		client.say(channel, `@${tags.username}, heya!`);
+	}
+});
